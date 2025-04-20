d547d4df03475c1021d206df36eadee2fb535243
hayleepierce <pierce03@allegheny.edu>
2025-04-20 17:53:04 -0400

    test: using jsdom

--- test/carousel.test.js
+++ test/carousel.test.js
@@ -1,7 +1,36 @@
+const { JSDOM } = require("jsdom");
+const fs = require("fs");
+const path = require("path");
+
+// Read HTML content
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
+// Read JavaScript content
+const js = fs.readFileSync(path.resolve(__dirname, "../assets/js/carousel.js"), "utf8");
+
+let dom;
+let document;
+
 describe('moveCarousel', () => {
     let carouselContainer;
 
     beforeEach(() => {
+        // Create a new JSDOM instance with the HTML content
+        dom = new JSDOM(html, {
+            runScripts: "dangerously",
+            resources: "usable",
+            url: "http://localhost"
+        });
+        document = dom.window.document;
+        
+        // Execute the script in the context of the JSDOM window
+        const scriptElement = document.createElement("script");
+        scriptElement.textContent = js;
+        document.body.appendChild(scriptElement);
+        
+        // Ensure DOMContentLoaded has fired
+        const event = new dom.window.Event('DOMContentLoaded');
+        dom.window.document.dispatchEvent(event);
+        
         // Set up a mock DOM structure for the carousel
         document.body.innerHTML = `
             <div class="carousel">
